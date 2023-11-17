126960a6ee8fe604d1d6f84fea5b8067f7b4c4d0
MevWebDev <szymongrysiewicz@gmail.com>
2023-11-17 16:31:10 +0100

    X

--- zadania/zadania_5/zad13.py
+++ zadania/zadania_5/zad13.py
@@ -4,7 +4,7 @@ list1 = []
 list2 = []
 start = random.randint(1,20)
 l = random.randint(1,20)
-print(start)
+
 for x in range (l):
     list1.append(start + x)
 toAppend = list1[-1]
@@ -12,3 +12,6 @@ for y in range(l):
     list2.append(toAppend +1 + y)
 matrix = [list1, list2]
 print(matrix)
+print("Połączone: ")
+list1.extend(list2)
+print(list1)
