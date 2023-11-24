cfe2004e8fd5cdba209bb4ec7ee545861fc64e5c
MevWebDev <szymongrysiewicz@gmail.com>
2023-11-24 16:33:08 +0100

    Dziala 4

--- zadania/zadania_7/zad4.py
+++ zadania/zadania_7/zad4.py
@@ -1,7 +1,7 @@
 
 import math
 from itertools import count
-list = [1,2,3,5,5,5,5,7,8,9]
+list = [1,2,3,5,5,5,5,5,5,7,8,9,10,11]
 dlugosc = int(math.log(len(list),2))
 x = 5
 def szukajX(x,lista):
@@ -16,13 +16,12 @@ def szukajX(x,lista):
         mid=(low+high)//2
         i = 0
         if list[mid]==x:
-            for i in range(1,len(lista)):
+            for i in range(0,len(lista)):
                 
-                if list[mid] != list[mid+i]:
-                    
-                    return mid
-                else:
-                    mid+=1
+                
+                if list[mid+i] != list[mid+i+1]:
+                    return mid+i
+                
                     
 
         elif list[mid]>x:
